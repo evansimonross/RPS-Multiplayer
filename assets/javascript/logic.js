8daf8c5f79f79cfac5004fbd2e0beee7a074e21f
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-07-22 13:06:28 -0400

    Added player 1 and player 2 headers

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyBaVG_WIYrk2Hk0ifu1ot-ISmlHD1G0sDY",
+    authDomain: "rps-multiplayer-4294a.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-4294a.firebaseio.com",
+    projectId: "rps-multiplayer-4294a",
+    storageBucket: "rps-multiplayer-4294a.appspot.com",
+    messagingSenderId: "618197940732"
+  };
+firebase.initializeApp(config);
+var database = firebase.database();
