48558ea796df19d37e0fd63b98b051cc12d0aff6
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-07-23 12:08:33 -0400

    Changed connections method to include a lobby... Total rewrite to avoid some issues.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -13,10 +13,12 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var connectionsRef = database.ref("/connections");
 var connectedRef = database.ref(".info/connected");
-var gamesRef = database.ref("/games");
+//var gamesRef = database.ref("/games");
 var player = {};
-var game = "";
-var gameRef;
+var opponent = {};
+var game = "lobby";
+var gamesRef = database.ref("/games");
+var gameStarted = false;
 
 // on user's connection status change
 connectedRef.on("value", function (snapshot) {
@@ -26,62 +28,63 @@ connectedRef.on("value", function (snapshot) {
         player.name = prompt("What is your name?");
         $('#player-1-name').text(player.name);
         con.onDisconnect().remove();
+        var me = gamesRef.child(game).child(player.id);
+        me.update({ name: player.name, waiting: false, newGame: "lobby"});
+        me.onDisconnect().remove();
+    }
+});
 
-        gamesRef.once("value").then(function (snapshot) {
-            var games = snapshot.val() || {};
-            var gameKeyValues = Object.entries(games);
-            for (var i = 0; i < gameKeyValues.length; i++) {
-                if (gameKeyValues[i][1].players.length === 1) {
-                    // join existing game
-                    gameKeyValues[i][1].players.push(player);
-                    gamesRef.set(games);
-                    $('#player-2-name').text(gameKeyValues[i][1].players[0].name);
-                    game = gameKeyValues[i][0];
-                    gamesRef.onDisconnect().set(snapshot.val());
+gamesRef.on("value", function (snapshot) {
+    var players = snapshot.val()[game];
+    if (game === "lobby") {
+        var me = players[player.id] || "";
+        if(me === ""){ return; }
+        if( me.newGame !== "lobby"){
+            game = me.newGame;
+            gamesRef.child("lobby").child(player.id).remove();
+            var meInGame = gamesRef.child(game).child("players").child(player.id);
+            meInGame.onDisconnect().remove();
+            gameStarted = true;
+            return;
+        }
+        playerIds = Object.keys(players);
+        for (var i = 0; i < playerIds.length; i++) {
+            if (playerIds[i] != player.id) {
+                if(players[playerIds[i]].waiting===true){
+                    gamesRef.child("lobby").child(player.id).remove();
+                    var newGamePlayers = {};
+                    newGamePlayers[player.id] = {name: player.name};
+                    newGamePlayers[playerIds[i]] = {name: players[playerIds[i]].name};
+                    var newGame = gamesRef.push({players: newGamePlayers});
+                    game = newGame.key;
+                    gamesRef.child("lobby").child(playerIds[i]).update({
+                        waiting: false,
+                        newGame: game
+                    });
+                    var meInGame = gamesRef.child(game).child("players").child(player.id);
+                    meInGame.onDisconnect().remove();
                     return;
                 }
             }
-            // create a new game
-            var newGame = [];
-            newGame.push(player);
-            myGame = gamesRef.push({ players: newGame });
-            game = myGame.key;
-            // delete games[game];
-            gamesRef.onDisconnect().set(games);
-        });
+        }
+        gamesRef.child(game).child(player.id).update({ waiting: true });
     }
+    else if(Object.keys(players['players']).length===1 && gameStarted) {
+        // return to lobby
+        gameStarted = false;
+        gamesRef.child(game).remove();
+        game = "lobby";
+        var me = gamesRef.child(game).child(player.id);
+        me.update({ name: player.name, waiting: true, newGame: "lobby"});
+        me.onDisconnect().remove();
+    }
+    else if(Object.keys(players['players']).length===2 && !gameStarted){
+        gameStarted = true;
+    }
+
 });
 
 // on any connection status change
 connectionsRef.on("value", function (snapshot) {
     $('#player-count').text(snapshot.numChildren());
-});
-
-// Check if other player has disconnected or joined
-gamesRef.on("value", function (snapshot) {
-    console.log("GAMES HAVE UPDATED")
-    if(game==="") { return; }
-    var games = snapshot.val();
-    var thisGame = snapshot.val()[game] || undefined;
-    if (thisGame === undefined) { return; }
-    console.log(thisGame);
-    if (thisGame.players.length === 1) {
-        // Other player has disconnected! 
-        $('#player-2-name').text("Player 2");
-        delete games[game];
-        gamesRef.onDisconnect().set(games);
-    }
-    else if (thisGame.players.length === 2) {
-        // Other player has joined.
-        if(thisGame.players[0].id===player.id){
-            $('#player-2-name').text(thisGame.players[1].name);
-            games[game].players = ([thisGame.players[1]]);
-            gamesRef.onDisconnect().set(games);
-        }
-        else{
-            $('#player-2-name').text(thisGame.players[0].name);
-            games[game].players = ([thisGame.players[0]]);
-            gamesRef.onDisconnect().set(games);
-        }
-    }
 });
